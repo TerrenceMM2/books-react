66fc14860ade07124ad632721c9d921621d17b5a
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-12 09:49:37 -0600

    feat: install and configure TanStack Router; configure devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
+// TanStack Router - https://tanstack.com/router/latest/docs/framework/react/guide/file-based-routing#configuration-with-vite
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite(),
+    react()
+  ],
 })
