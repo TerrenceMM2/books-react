0a47baebbc8f6d5365d5b6d1bf80a6ed3e814b59
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-12 11:43:56 -0600

    test: add mock values

---
+++ src/__mock__/ReviewResponse.ts
@@ -0,0 +1,6 @@
+export const mockReview = {
+  id: "abc123",
+  bookId: "Uu3yCAAAQBAJ",
+  reviewText: "This book is great",
+  starRating: 5
+}
---
+++ src/__mock__/index.ts
@@ -0,0 +1,4 @@
+import { mockReview } from "./ReviewResponse";
+import { mockVolume, createMockVolumeList } from "./VolumeResponse";
+
+export { mockReview, mockVolume, createMockVolumeList }
