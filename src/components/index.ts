10470e7f5b9be5b44d398c8a0519938227a1ee8c
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-12 11:43:28 -0600

    chore: commit component index

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { DetailsContainer, ResultsContainer, SearchContainer } from "./containers"
+import { Button, SearchBar } from "./elements"
+
+
+export { Button, SearchBar, DetailsContainer, SearchContainer, ResultsContainer}
