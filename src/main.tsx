37dd62f0ecbdad2408a2067decc2c83ea5792943
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-12 19:03:17 -0600

    feat: add tanstack query and devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import './main.css'
+import { routeTree } from "./routeTree.gen.ts";
 
-import { routeTree } from './routeTree.gen.ts'
+import "./main.css";
 
 // https://tanstack.com/router/latest/docs/framework/react/quick-start#srcmaintsx
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
+// https://tanstack.com/query/latest/docs/framework/react/quick-start
+const queryClient = new QueryClient();
+
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-  const root = createRoot(rootElement)
+  const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
-  )
+  );
 }
