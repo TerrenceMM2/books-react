c2b7fc459be3d60b0669af78cf7cd90e0de234c2
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-12 22:56:49 -0600

    feat: use mutation state to display search results

--- src/components/containers/ResultsContainer.tsx
+++ src/components/containers/ResultsContainer.tsx
@@ -1,12 +1,20 @@
-import { createMockVolumeList } from '../../__mock__'
-import { Result } from '../modules';
-
-const volumeList = createMockVolumeList();
+import { useMutationState } from "@tanstack/react-query";
+import { Result } from "../modules";
+import { Volume } from "../../api";
 
 const ResultsContainer = () => {
-  return (
-    <div>{volumeList.map((volume, index) => <Result key={`${volume.id}-${index}`} result={volume} />)}</div>
-  )
-}
+  const results = useMutationState({
+    filters: { mutationKey: ["searchResults"] },
+    select: (store) => store.state.data as Volume[],
+  });
 
-export default ResultsContainer
+  return (
+    <div>
+      {results[0]?.length > 0
+        ? results[0].map((volume, index) => <Result key={`${volume.id}-${index}`} result={volume} />)
+        : null}
+    </div>
+  );
+};
+
+export default ResultsContainer;
