7e6462711a26e864342526088dd1ca7f6f6a6e0a
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-12 22:55:32 -0600

    feat: implement mutation to execute book search

--- src/components/containers/SearchContainer.tsx
+++ src/components/containers/SearchContainer.tsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
-import Input from "../elements/Input";
+import { useMutation } from "@tanstack/react-query";
+
+import { Button, Input } from "../elements";
+import { getBookSearch } from "../../api";
 
 const SearchContainer = () => {
-  const [, setSearchTerm] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const { mutate } = useMutation({
+    mutationKey: ["searchResults"],
+    mutationFn: (searchTerm: string) => getBookSearch(searchTerm),
+  });
 
   const handleOnChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(value);
   };
 
-  return <Input name="search" label="Book Search" onChange={handleOnChange} />;
+  return (
+    <>
+      <Input name="search" label="Book Search" onChange={handleOnChange} />
+      <Button label="search" onClick={() => mutate(searchTerm)} />
+    </>
+  );
 };
 
 export default SearchContainer;
