791bbbad9f6472478507a8b5d0817a84a134a43b
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-14 15:13:20 -0600

    feat: query review endpoint using volumeId parameters

--- src/components/containers/ReviewsContainer.tsx
+++ src/components/containers/ReviewsContainer.tsx
@@ -1,19 +1,28 @@
-import { mockReview } from "../../__mock__";
+import { FC } from "react";
+import { useParams } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+
 import Review from "../modules/Review";
 import ReviewFormContainer from "./ReviewFormContainer";
+import { getBookReviews } from "../../api/fetch";
+import type { Review as ReviewType } from "../../api";
 
-const review = mockReview;
+const ReviewsContainer: FC = () => {
+  const { volumeId } = useParams({ strict: false });
 
-const ReviewsContainer = () => {
-  // Call GET /reviews HERE
+  const { data, isLoading, error } = useQuery({
+    queryKey: [`${volumeId}-reviews`],
+    queryFn: () => getBookReviews(volumeId),
+    enabled: !!volumeId,
+  });
 
   return (
     <>
-      {Array(3)
-        .fill(review)
-        .map((review, index) => (
-          <Review key={`${review.id}-${index}`} review={review} />
-        ))}
+      {typeof data === "string"
+        ? null
+        : data?.map((review: ReviewType, index: number) => <Review key={`${review.id}-${index}`} review={review} />)}
+      {isLoading && "...loading"}
+      {error && "ERROR"}
       <ReviewFormContainer />
     </>
   );
