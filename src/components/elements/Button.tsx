9399ab3413dc7ac494fc7826af65e36db13fa7a0
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-14 13:52:48 -0600

    feat: add disabled feature and styling

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -2,12 +2,16 @@ import { FC, ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
+  disabled?: boolean;
+  className?: string;
   onClick: () => void;
 }
 
-const Button: FC<ButtonProps> = ({ label, onClick }) => {
+const Button: FC<ButtonProps> = ({ label, disabled = false, className, onClick }) => {
+  const disabledStyles = disabled ? "text-gray-600" : "";
+
   return (
-    <button onClick={onClick} type="button">
+    <button disabled={disabled} className={`${disabledStyles} ${className}`} onClick={onClick} type="button">
       {label}
     </button>
   );
