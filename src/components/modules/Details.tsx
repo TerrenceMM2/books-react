b71aa023d204a4b74e08289e4255e84778202ebd
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-14 15:13:44 -0600

    refactor: update style; include react typing

--- src/components/modules/Details.tsx
+++ src/components/modules/Details.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import { Link } from "@tanstack/react-router";
 
 import { Volume } from "../../api";
@@ -6,20 +7,22 @@ interface DetailsProps {
   volume: Volume;
 }
 
-const Details = ({ volume: { authors, description, imageLink, infoLink, publisher, title } }: DetailsProps) => {
+const Details: FC<DetailsProps> = ({
+  volume: { authors = [], description, imageLink, infoLink, publisher, title },
+}) => {
   return (
     <div className="text-left">
-      <img className="m-auto pb-4 w-3/5 max-w-64" src={imageLink} alt={`${title} book cover`} />
+      <img className="m-auto mb-4 w-3/5 max-w-64" src={imageLink} alt={`${title} book cover`} />
       <Link to={infoLink} target="_blank">
-        <p className="text-xl pb-4">{title}</p>
+        <p className="text-xl mb-4">{title}</p>
       </Link>
       {authors.map((author, index) => (
-        <p key={`${title}-${author}-${index}`} className="font-bold pb-4">
+        <p key={`${title}-${author}-${index}`} className="font-bold mb-4">
           {author}
         </p>
       ))}
-      <p className="pb-4">{publisher}</p>
-      <p className="pb-4">{description}</p>
+      <p className="mb-4">{publisher}</p>
+      <p className="mb-4">{description}</p>
     </div>
   );
 };
