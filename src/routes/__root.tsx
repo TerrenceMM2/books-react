b13f927dfaf2c88d5eed0f00929eb43d0c2cbc8e
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-15 14:17:44 -0600

    ci: change environment reference per vitejs docs

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,17 +3,16 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import React, { Suspense } from "react";
 
 // https://tanstack.com/router/latest/docs/framework/react/devtools#only-importing-and-using-devtools-in-development
-const TanStackRouterDevtools =
-  import.meta.env.NODE_ENV === "production"
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
-        // Lazy load in development
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel,
-        })),
-      );
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel,
+      })),
+    );
 
 export const Route = createRootRoute({
   component: () => (
