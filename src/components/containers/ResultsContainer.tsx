8d9feae0f3f19779f5cdef228971f27b4eefad24
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-18 22:22:50 -0600

    refactor: use search context to get query data

--- src/components/containers/ResultsContainer.tsx
+++ src/components/containers/ResultsContainer.tsx
@@ -1,18 +1,18 @@
 import type { FC } from "react";
-import { useMutationState } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { Result } from "../modules";
-import { Volume } from "../../api";
+import { useSearchContext } from "../context/searchContext";
+import type { Volume } from "../../api";
 
 const ResultsContainer: FC = () => {
-  const results = useMutationState({
-    filters: { mutationKey: ["searchResults"] },
-    select: (store) => store.state.data as Volume[],
-  });
+  const { searchTerm, page } = useSearchContext();
+  const { data } = useQuery({ queryKey: [`${searchTerm}-results`, page] });
+  const results = data as Volume[];
 
   return (
-    <div>
-      {results[results.length - 1]?.length > 0
-        ? results[results.length - 1].map((volume, index) => <Result key={`${volume.id}-${index}`} result={volume} />)
+    <div className="pt-6">
+      {results?.length > 0
+        ? results.map((volume, index) => <Result key={`${volume.id}-${index}`} result={volume} />)
         : null}
     </div>
   );
