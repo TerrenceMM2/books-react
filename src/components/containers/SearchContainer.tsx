2b7b922e12285d665c2d720fa00924a26b877440
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-18 22:23:13 -0600

    refactor: use search context; change mutation to query; change term key

--- src/components/containers/SearchContainer.tsx
+++ src/components/containers/SearchContainer.tsx
@@ -1,29 +1,57 @@
-import { useState } from "react";
-import type { FC } from "react";
-import { useMutation } from "@tanstack/react-query";
+import type { FC, ReactNode } from "react";
+import { useQuery } from "@tanstack/react-query";
 
-import { Button, Input } from "../elements";
+import { Button, Input, LeftArrow, RightArrow } from "../elements";
 import { getBooksSearch } from "../../api";
+import { useSearchContext } from "../context/searchContext";
 
-const SearchContainer: FC = () => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const { mutate, error, isPending, reset } = useMutation({
-    mutationKey: ["searchResults"],
-    mutationFn: (searchTerm: string, page: number = 1, pageSize: number = 10) =>
-      getBooksSearch(searchTerm, page, pageSize),
+interface SearchContainerProps {
+  children: ReactNode;
+}
+
+const SearchContainer: FC<SearchContainerProps> = ({ children }) => {
+  const { searchTerm, setSearchTerm, page, setPage } = useSearchContext();
+
+  const { data, error, isFetching, refetch } = useQuery({
+    queryKey: [`${searchTerm}-results`, page],
+    queryFn: () => getBooksSearch(searchTerm, page),
+    enabled: false,
   });
 
   const handleOnChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
-    reset();
     setSearchTerm(value);
   };
 
+  const handlePageChange = (action: string) => {
+    if (action === "down" && page === 1) {
+      return;
+    }
+
+    if (action === "up") {
+      setPage(page + 1);
+    }
+
+    if (action === "down") {
+      setPage(page - 1);
+    }
+  };
+
   return (
-    <div className="mb-4 md:flex md:gap-4">
-      <Input name="search" onChange={handleOnChange}>
-        {error && <div className="text-left text-red-600">{error.message}</div>}
-      </Input>
-      <Button className="" disabled={isPending} label="Search" onClick={() => mutate(searchTerm)} />
+    <div className="mb-4 flex md:flex-col md:gap-4">
+      <div className="flex gap-4 ">
+        <Input name="search" onChange={handleOnChange}>
+          {error && <div className="text-left text-red-600">{error.message}</div>}
+        </Input>
+        <Button className="" disabled={isFetching} label="Search" onClick={refetch} />
+      </div>
+      {children}
+      {data && (
+        <div className="flex flex-row justify-center gap-4 pb-4 items-center">
+          <LeftArrow onClick={handlePageChange} />
+          {page}
+          <RightArrow onClick={handlePageChange} />
+        </div>
+      )}
     </div>
   );
 };
