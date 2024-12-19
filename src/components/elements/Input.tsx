3e2ba46a7647f27cb3afca334be6964d8940f107
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-18 19:45:31 -0600

    feat: add onKeyDown prop

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -1,15 +1,22 @@
-import { FC, ChangeEvent, InputHTMLAttributes } from "react";
+import { FC, InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   children?: React.ReactNode;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const Input: FC<InputProps> = ({ name, children, onChange }) => {
+const Input: FC<InputProps> = ({ name, children, onChange, onKeyDown }) => {
   return (
     <div className="mb-4 md:m-0 md:w-full">
-      <input className="p-2 rounded w-full h-10 md:h-full md:m-0" type="text" name={name} onChange={onChange} />
+      <input
+        className="p-2 rounded w-full h-10 md:h-full md:m-0"
+        type="text"
+        name={name}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
       {children}
     </div>
   );
