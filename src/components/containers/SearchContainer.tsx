8f1bf800363249cb2562cfa9b8c369ca05c058a3
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-18 19:45:19 -0600

    feat: add page and pageSize to mutation function; add handler for enter press

--- src/components/containers/SearchContainer.tsx
+++ src/components/containers/SearchContainer.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import type { FC } from "react";
 import { useMutation } from "@tanstack/react-query";
 
 import { Button, Input } from "../elements";
 import { getBooksSearch } from "../../api";
 
-const SearchContainer = () => {
+const SearchContainer: FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const { mutate, error, isPending, reset } = useMutation({
     mutationKey: ["searchResults"],
-    mutationFn: (searchTerm: string) => getBooksSearch(searchTerm),
+    mutationFn: (searchTerm: string, page: number = 1, pageSize: number = 10) =>
+      getBooksSearch(searchTerm, page, pageSize),
   });
 
   const handleOnChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,9 +18,16 @@ const SearchContainer = () => {
     setSearchTerm(value);
   };
 
+  const handleOnEnter = ({ key }: React.KeyboardEvent<HTMLInputElement>) => {
+    if (key === "Enter") {
+      reset();
+      mutate(searchTerm);
+    }
+  };
+
   return (
     <div className="mb-4 md:flex md:gap-4">
-      <Input name="search" onChange={handleOnChange}>
+      <Input name="search" onChange={handleOnChange} onKeyDown={handleOnEnter}>
         {error && <div className="text-left text-red-600">{error.message}</div>}
       </Input>
       <Button className="" disabled={isPending} label="Search" onClick={() => mutate(searchTerm)} />
