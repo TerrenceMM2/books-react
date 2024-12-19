eb6c4073db711eea189023b9ae75386271fc0a2d
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-18 19:44:53 -0600

    fix: return latest mutation results

--- src/components/containers/ResultsContainer.tsx
+++ src/components/containers/ResultsContainer.tsx
@@ -1,8 +1,9 @@
+import type { FC } from "react";
 import { useMutationState } from "@tanstack/react-query";
 import { Result } from "../modules";
 import { Volume } from "../../api";
 
-const ResultsContainer = () => {
+const ResultsContainer: FC = () => {
   const results = useMutationState({
     filters: { mutationKey: ["searchResults"] },
     select: (store) => store.state.data as Volume[],
@@ -10,8 +11,8 @@ const ResultsContainer = () => {
 
   return (
     <div>
-      {results[0]?.length > 0
-        ? results[0].map((volume, index) => <Result key={`${volume.id}-${index}`} result={volume} />)
+      {results[results.length - 1]?.length > 0
+        ? results[results.length - 1].map((volume, index) => <Result key={`${volume.id}-${index}`} result={volume} />)
         : null}
     </div>
   );
