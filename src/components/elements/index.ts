b7a4fb66e737d244e1f87188721b3a60b4178383
Terrence Mahnken <terrencemm2@gmail.com>
2024-12-18 20:11:39 -0600

    feat: add left and right arrow svgs

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -1,8 +1,10 @@
 import Button from "./Button";
 import Input from "./Input";
+import LeftArrow from "./LeftArrow";
 import MissingImage from "./MissingImage";
+import RightArrow from "./RightArrow";
 import StarInput from "./StarInput";
 import StarIcon from "./StarIcon";
 import Text from "./Text";
 
-export { Button, Input, MissingImage, StarInput, StarIcon, Text };
+export { Button, Input, LeftArrow, MissingImage, RightArrow, StarInput, StarIcon, Text };
